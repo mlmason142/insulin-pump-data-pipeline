f798adc6aa94bb0b6e10222f3ba28c6ee69cc6a7
Melissa Mason <melissamason@Melissas-Mac-mini.local>
2025-04-02 14:31:57 -0400

    separate out parsed data

--- dagster/alert_alarm.py
+++ dagster/alert_alarm.py
@@ -29,8 +29,34 @@ hourly_schedule = dg.ScheduleDefinition(
     cron_schedule="0 * * * *"
 )
 
+@dg.asset 
+
+def fetch_all_events() -> dg.MaterializeResult:
+    load_dotenv()
+    pump_id = os.getenv('PUMP_ID')
+    today = date.today()
+    print(today)
+    data = TandemSourceApi().pump_events(pump_id, today, today)
+    event_data = []
+    for e in data:
+        if isinstance (e, dict):
+            event_data.append(e)
+    df = ps.DataFrame(event_data)
+
+    return dg.MaterializeResult(metadata={"preview":dg.MetadataValue.md(df.to_markdown(index=False)),})
+
+daily_job = dg.define_asset_job(
+    "daily_job", selection=["fetch_all_events"]
+)
+
+daily_schedule = dg.ScheduleDefinition(
+    job=daily_job,
+    cron_schedule="0 0 * * *"
+)
+
+
 defs = dg.Definitions(
-    assets=[get_alert_alarm_data],
-    jobs=[hourly_alert_job],
-    schedules=[hourly_schedule],
+    assets=[get_alert_alarm_data, fetch_all_events],
+    jobs=[hourly_alert_job, daily_job],
+    schedules=[hourly_schedule, daily_schedule],
 )
